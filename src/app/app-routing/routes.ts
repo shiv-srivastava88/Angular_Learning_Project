4bb4eb4b82d9788db10bae7321ef7619b70cce7b
Shivansh Srivastava <shiv.srivastava88@gmail.com>
2020-05-07 01:43:36 +0530

    Angular Router Basics

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { HomeComponent } from '../home/home.component';
+import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+import { Component } from '@angular/core';
+
+export const routes : Routes = [
+    { path :'home' , component : HomeComponent},
+    { path :'menu' , component : MenuComponent},
+    { path : '' , redirectTo : '/home' , pathMatch : 'full'} 
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-import { DishService } from './services/dish.service';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +42,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
