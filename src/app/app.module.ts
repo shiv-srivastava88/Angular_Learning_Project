323453be4f25e661f48aa225d79862d844622949
Shivansh Srivastava <shiv.srivastava88@gmail.com>
2020-05-24 21:33:21 +0530

    Assigment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 
@@ -58,6 +59,7 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatSliderModule,
     AppRoutingModule,
     MatDialogModule,
     MatFormFieldModule,
--- src/app/dishdetails/dishdetails.component.ts
+++ src/app/dishdetails/dishdetails.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment'; 
 
-import { Params, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute ,Params } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder , FormGroup , Validators, Form } from '@angular/forms';
 
 
 @Component({
@@ -16,6 +18,10 @@ import { switchMap } from 'rxjs/operators';
 
 export class DishdetailsComponent implements OnInit {
 
+  @ViewChild('cform') commentFormDirective;
+
+  commentForm : FormGroup;
+  comment: Comment;
 
   dish : Dish;
   dishIds: string[];
@@ -24,7 +30,27 @@ export class DishdetailsComponent implements OnInit {
 
   constructor( private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
+
+    formErrors = {
+      'author': '',
+      'comment' : '',
+      'rating' : 5,
+    };
+  
+    validationMessages = {
+      'author': {
+        'required':      'Author Name is required.',
+        'minlength':     'Author Name must be at least 2 characters long.',
+        'maxlength':     'Author Name cannot be more than 25 characters long.'
+      },
+      'comment': {
+        'required':      'Comment is required.'
+      },
+    };  
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -42,4 +68,50 @@ export class DishdetailsComponent implements OnInit {
     this.location.back();
   }
 
+  createForm(){
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      comment: ['' , Validators.required],
+      rating:5
+    });
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data? : any){
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    this.dish.comments.push(this.comment);
+    console.log(this.comment);
+    this.commentForm.reset({
+      author: '',
+      comment: '',
+      rating:5
+
+    });
+    this.commentFormDirective.resetForm();
+  }
 }
